badda7fce6c4d4714f1fdf29a08a13813f5161a1
Tung Nguyen <tongueroo@gmail.com>
2017-06-02 16:25:11 -0700

    improve jack eb install error message

--- lib/jack/version_checker.rb
+++ lib/jack/version_checker.rb
@@ -3,12 +3,13 @@ module Jack
     REQUIRED_VERSION = "3.1.2"
 
     def run
-      leave("eb cli tool is not installed") unless system("type eb > /dev/null 2>&1")
-      leave("eb version is too low") unless check
+      leave(not_installed) unless system("type eb > /dev/null 2>&1")
+      leave(version_too_low) unless check
+      # "SORRY: #{message}, please install at least version #{REQUIRED_VERSION}")
     end
 
     def check
-      major, minor, patch = parse_version(get_version)
+      major, minor, patch = normalize_version(parsed_version)
       r_major, r_minor, r_patch = normalize_version(REQUIRED_VERSION)
       (major > r_major) ||
       (major == r_major && minor > r_minor) ||
@@ -19,18 +20,45 @@ module Jack
       `eb --version`
     end
 
+    def parsed_version
+      @parsed_version ||= parse_version(get_version)
+    end
+
     def parse_version(version)
       parsed = version.match(/EB CLI (\d+\.\d+\.\d+)/)[1]
-      normalize_version(parsed)
     end
+
 
     def normalize_version(parsed)
       parsed.split('.').collect(&:to_i)
     end
 
+    def not_installed
+      message = "Unable to detect an installation of the eb cli tool. Please install the eb tool.\n\n"
+      message << install_instructions
+      message
+    end
+
+    def version_too_low
+      <<~EOS
+        Unable to detect a version of the eb cli tool that works with jack.
+        Detected version #{parsed_version}.
+
+        #{install_instructions}
+      EOS
+    end
+
+    def install_instructions
+      if RUBY_PLATFORM =~ /darwin/
+        "You can install the eb tool via homebrew:\n\nbrew install awsebcli"
+      else
+        "Installation instructions: http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/eb-cli3-install.html"
+      end
+    end
+
     # for specs
-    def leave(message='')
-      puts("SORRY: #{message}, please install at least version #{REQUIRED_VERSION}")
+    def leave(message)
+      puts(message)
       exit 0
     end
   end
