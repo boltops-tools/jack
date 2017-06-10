0984e03e2b0d8ccd769681a3bd8a6d518dc02f18
Tung Nguyen <tongueroo@gmail.com>
2017-06-09 20:10:08 -0700

    flatten cli interface

--- lib/jack/cli.rb
+++ lib/jack/cli.rb
@@ -4,35 +4,6 @@ require 'jack/version_checker'
 Jack::VersionChecker.new.run unless ENV['TEST']
 
 module Jack
-  class Config < Command
-    desc "apply ENV_NAME", "apply jack config changes to EB environment"
-    long_desc Help.apply
-    option :sure, aliases: :f, type: :boolean, desc: "skip prompt"
-    def apply(env_name)
-      Apply.new(options.merge(env_name: env_name)).run
-    end
-
-    desc "get ENV_NAME", "downloads environment config to jack/cfg folder"
-    long_desc Help.get
-    option :dirty, type: :boolean, desc: "leave the remote eb config and downloaded config behind"
-    def get(env_name)
-      Get.new(options.merge(env_name: env_name)).run
-    end
-
-    desc "diff ENV_NAME", "diff jack config vs environment config"
-    long_desc Help.diff
-    option :dirty, type: :boolean, desc: "leave the remote eb config and downloaded config behind"
-    def diff(env_name)
-      Diff.new(options.merge(env_name: env_name)).run
-    end
-
-    desc "sort ENV_NAME", "reformat local jack config file to a sorted yaml file format"
-    long_desc Help.sort
-    def sort(env_name)
-      Sort.new(options.merge(env_name: env_name)).run
-    end
-  end
-
   class CLI < Command
     class_option :verbose, type: :boolean
     class_option :mute, type: :boolean, desc: "mute all output, useful for specs"
@@ -60,13 +31,36 @@ module Jack
       Terminate.new(options.merge(env_name: env_name)).run
     end
 
+    desc "apply ENV_NAME", "apply jack config changes to EB environment"
+    long_desc Config::Help.apply
+    option :sure, aliases: :f, type: :boolean, desc: "skip prompt"
+    def apply(env_name)
+      Config::Apply.new(options.merge(env_name: env_name)).run
+    end
+
+    desc "get ENV_NAME", "downloads environment config to jack/cfg folder"
+    long_desc Config::Help.get
+    option :dirty, type: :boolean, desc: "leave the remote eb config and downloaded config behind"
+    def get(env_name)
+      Config::Get.new(options.merge(env_name: env_name)).run
+    end
+
+    desc "diff ENV_NAME", "diff jack config vs environment config"
+    long_desc Config::Help.diff
+    option :dirty, type: :boolean, desc: "leave the remote eb config and downloaded config behind"
+    def diff(env_name)
+      Config::Diff.new(options.merge(env_name: env_name)).run
+    end
+
+    desc "sort ENV_NAME", "reformat local jack config file to a sorted yaml file format"
+    long_desc Config::Help.sort
+    def sort(env_name)
+      Config::Sort.new(options.merge(env_name: env_name)).run
+    end
+
     desc "version", "display jack version number"
     def version
       puts VERSION
     end
-
-    desc "config ACTION ENV_NAME", "manage environment config"
-    long_desc Help.config
-    subcommand "config", Config
   end
 end
